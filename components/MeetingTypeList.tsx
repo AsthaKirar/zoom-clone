2e69ed12083e5c1724dd69c421df38c0f0f67599
AsthaKirar <asthap602@gmail.com>
2025-04-09 22:07:16 +0530

    commit

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+"use client"
+import { useState } from 'react'
+import HomeCard from './ui/HomeCard'
+import { useRouter } from 'next/navigation'
 
 const MeetingTypeList = () => {
+    const route = useRouter();
+    const [meetingState, setmeetingState] = useState<'isSchedulMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
-        <div className='bg-[#FF742E] px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px]'>
-            Box 1
-        </div>
+        <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description = "Start an instant meeting"
+        handleClick={() => setmeetingState('isJoiningMeeting')}
+        className="bg-[#FF742E]"/>
+        <HomeCard
+        
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description = "Planing your meeting"
+        handleClick={() => setmeetingState('isSchedulMeeting')}
+        className="bg-[#0E78F9]"/>
+        <HomeCard 
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description = "Check out your recordings"
+        handleClick={() => setmeetingState('isJoiningMeeting')}
+        className="bg-[#830EF9]"/>
+        <HomeCard 
+        
+        img="/icons/join-meeting.svg"
+        title="New Meeting"
+        description = "via invitation"
+        handleClick={() => setmeetingState('isJoiningMeeting')}
+        className="bg-[#F9A90E]"/>
+
     </section>
   )
 }
